362823e58f4fd4b58d9e6c4ca1561685bff27e4b
grimreaper0999 <katper0610003@gmail.com>
2025-01-07 21:11:17 +0100

    dodan annotation za path v application

--- api/src/main/java/api/v1/parkingadvisorApplication.java
+++ api/src/main/java/api/v1/parkingadvisorApplication.java
@@ -6,6 +6,7 @@ import org.ekipaenajst.entitete.Parkirisce;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
@@ -17,6 +18,7 @@ public class parkingadvisorApplication extends Application {
     private SortLogic sortlogic;
 
     @GET
+    @Path("{params}")
     // npr. blabla/advisor?params=oddaljenost,cena_default,cena_short
     public Response vrniSortedParkirišča(@PathParam("params") List<String> sortparams){
 
